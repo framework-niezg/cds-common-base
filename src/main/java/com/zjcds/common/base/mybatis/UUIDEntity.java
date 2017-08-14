57017af9b852ee3729a320d4d7aaf67c60d945cf
niezg <niezg@zjcds.com>
2017-08-14 14:46:29 +0800

    修改为jpa支持ID实体

--- src/main/java/com/zjcds/common/base/mybatis/UUIDEntity.java
+++ src/main/java/com/zjcds/common/base/mybatis/UUIDEntity.java
@@ -5,14 +5,19 @@ import com.zjcds.common.base.utils.UUIDUtils;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 /**
  * created date：2017-08-07
  * @author niezhegang
  */
 @Getter
 @Setter
+@MappedSuperclass
 public abstract class UUIDEntity extends BaseBean{
     //初始化赋值一个uuid，等同于为新建实体对象赋一个id值
+    @Id
     private String id = UUIDUtils.randomUUID();
 
 }
